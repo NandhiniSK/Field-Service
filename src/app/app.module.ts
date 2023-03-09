8d8402b120b6d1fee10e988f8d562c2b52e5d0b1
NandhiniSK <51111386+NandhiniSK@users.noreply.github.com>
2023-03-09 14:28:29 +0530

    6 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,60 @@ import { InvoiceComponent } from './invoice/invoice.component';
 import { HistoryComponent } from './history/history.component';
 import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
 import { ServiceListComponent } from './service-list/service-list.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatRippleModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot([])],
+  imports: [BrowserModule, FormsModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTooltipModule,
+     RouterModule.forRoot([])],
   declarations: [
     AppComponent,
-
     CustomerComponent,
     EmployeeComponent,
     HistoryComponent,
